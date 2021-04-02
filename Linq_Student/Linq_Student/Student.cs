020c64acb294e56014ddc31ca52a4de0110fbfdf
MPozd <47981972+MPozd@users.noreply.github.com>
2021-04-02 19:37:01 +0300

    Пр7

---
+++ Linq_Student/Linq_Student/Student.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Student { 
+    public string First { get; set; }
+    public string Last { get; set; } 
+    public int ID { get; set; } 
+    public List<int> Scores; 
+}
